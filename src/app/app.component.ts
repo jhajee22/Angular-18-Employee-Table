48d500bd0c35866b6206369ded13807980d34b82
jhajee22 <kumarsaurav565@gmail.com>
2025-03-17 14:55:33 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
+import { DragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RolesComponent,],
+  imports: [RouterOutlet,RolesComponent,DragDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop,moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule,DragDropModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
@@ -25,23 +27,34 @@ export class RolesComponent {
   message: string = "Welcome to the world of Angular"
 
   users = [
-    { id: 1, name: 'Alice', age: 25, role: 'Admin' },
-    { id: 2, name: 'Bob', age: 28, role: 'Manager' },
-    { id: 3, name: 'Charlie', age: 22, role: 'Developer' },
-    { id: 4, name: 'David', age: 30, role: 'Tester' }
+    { id: 'Emp01', name: 'Alice', age: 25, role: 'Admin' },
+    { id: 'Emp02', name: 'Bob', age: 28, role: 'Manager' },
+    { id: 'Emp03', name: 'Charlie', age: 22, role: 'Developer' },
+    { id: 'Emp04', name: 'David', age: 30, role: 'Tester' }
   ];
+  // displayedColumn:string[] = ['Sr.No','id','name','age','role']
 
   newUser = {
-    id: 0, name: '', age: 0, role: '',
+    id: '', name: '', age: 0, role: '',
   }
 
   addEmployee() {
-    this.users.push(this.newUser)
-    // this.newUser = { 
-    //   id:0, name:'',age:0 ,role:'',
-    // }
+    this.users.push({...this.newUser})
+    
   }
-  deleteEmployee(){
-    this.users.pop()       
+
+  deleteEmployee(index:number) {
+    this.users.splice(index,1)
   }
+
+  // modifyEmployee() {
+  // }
+
+  drop(event:CdkDragDrop<[]>){
+moveItemInArray(this.users,event.previousIndex,event.currentIndex);
+  }
+
 }
+
+
+
