2b50ecf1c534b8d1efd9812a719ca45dd1baafa4
jhajee22 <kumarsaurav565@gmail.com>
2025-03-18 14:50:27 +0530

    changes on 18/03/2025

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -2,59 +2,57 @@ import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { CdkDragDrop,moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule,DragDropModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, DragDropModule],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
 //Typescript class
 export class RolesComponent {
   //Typescript Data Types
   //string,number,boolean,date,object,array,null,undefined
   //creating variable
-  //Now if we want to see these variable on our page 
+  //Now if we want to see these variable on our page
   //For that purpose we need to do Data Binding
-  firstName: string = "Creating Angular version-18 project"
-  angularVersion = "version-18";
+  firstName: string = 'Creating Angular version-18 project';
+  angularVersion = 'version-18';
   version: number = 18;
   isActive: boolean = false;
   currentDate: Date = new Date();
-  inputType: string = "button";
-  message: string = "Welcome to the world of Angular"
+  inputType: string = 'button';
+  message: string = 'Welcome to the world of Angular';
 
   users = [
     { id: 'Emp01', name: 'Alice', age: 25, role: 'Admin' },
     { id: 'Emp02', name: 'Bob', age: 28, role: 'Manager' },
     { id: 'Emp03', name: 'Charlie', age: 22, role: 'Developer' },
-    { id: 'Emp04', name: 'David', age: 30, role: 'Tester' }
+    { id: 'Emp04', name: 'David', age: 30, role: 'Tester' },
   ];
   // displayedColumn:string[] = ['Sr.No','id','name','age','role']
 
   newUser = {
-    id: '', name: '', age: 0, role: '',
-  }
+    id: '',
+    name: '',
+    age: 0,
+    role: '',
+  };
 
   addEmployee() {
-    this.users.push({...this.newUser})
-    
+    this.users.push({ ...this.newUser });
   }
 
-  deleteEmployee(index:number) {
-    this.users.splice(index,1)
+  deleteEmployee(index: number) {
+    this.users.splice(index, 1);
   }
 
   // modifyEmployee() {
   // }
 
-  drop(event:CdkDragDrop<[]>){
-moveItemInArray(this.users,event.previousIndex,event.currentIndex);
+  drop(event: CdkDragDrop<[]>) {
+    moveItemInArray(this.users, event.previousIndex, event.currentIndex);
   }
-
 }
-
-
-
